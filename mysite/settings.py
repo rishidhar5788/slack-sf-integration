8032ffec669bb82bccc51596804046843b4e6b56
rishidhar5788 <rishidhar5788@gmail.com>
2021-09-27 23:45:25 +0100

    Remove Django app Secret Key

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(override=True)
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
